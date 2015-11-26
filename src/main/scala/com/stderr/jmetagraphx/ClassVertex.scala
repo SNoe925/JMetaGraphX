57d1a0413ed299de92ebfff528fd78983dfcc238
Shannon Noe <shannon_noe@yahoo.com>
2015-11-26 11:48:52 -0600

    Silence log4j Report top 10 called classes

--- src/main/scala/com/stderr/jmetagraphx/ClassVertex.scala
+++ src/main/scala/com/stderr/jmetagraphx/ClassVertex.scala
@@ -12,7 +12,7 @@ object ClassVertex {
   private val classVertexMap: mutable.Map[String, (VertexId, ClassVertex)] = mutable.HashMap()
 
   def getOrElseUpdate(name: String, jarId:Integer = 0) = {
-    val id: VertexId = jarId << 48 + classVertexMap.size + 1
+    val id: VertexId = (jarId << 48) + classVertexMap.size + 1
     classVertexMap.getOrElseUpdate(name.intern(), new ClassVertex(id, name).toPair)
   }
 
--- src/main/scala/com/stderr/jmetagraphx/Main.scala
+++ src/main/scala/com/stderr/jmetagraphx/Main.scala
@@ -2,12 +2,13 @@ package com.stderr.jmetagraphx
 
 import java.io.File
 
-import org.apache.log4j.Logger
+import org.apache.log4j.{Level, Logger, BasicConfigurator}
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.graphx._
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SQLContext
+
+import org.apache.commons.io.FileUtils
 
 object Main {
 
@@ -18,23 +19,36 @@ object Main {
   val edgeFilePath: String = s"$homeDirectory/edge.dat"
 
   def main(args: Array[String]) {
+    // Quiet the defaults from spark. Use INFO for development
+    BasicConfigurator.configure()
+    Logger.getRootLogger.setLevel(Level.ERROR)
+
+    cleanupFiles
+
+    // Extract the method call information from jar files
+    scanJars(args)
+
+    // Create Spark
     val conf = new SparkConf().setAppName("JMetaGraphX").setMaster("local")
     val sc = new SparkContext(conf)
-    val sqlContext = new SQLContext(sc)
+    // Create RDDs from the scanned classes
+    val vertexRDD = sc.parallelize(ClassVertex.toSeq)
+    val edgeRDD = sc.parallelize(MethodCall.toSeq)
 
+    // Save the RDDs for use int the spark shell
+    vertexRDD.saveAsObjectFile(vertexFilePath)
+    edgeRDD.saveAsObjectFile(edgeFilePath)
+
+    // Analysis
+    run(sc)
+  }
+
+  def scanJars(args: Array[String]) = {
     val mvnRepo = s"$homeDirectory/.m2/repository"
     val directoryToScan = if (args.isEmpty) new File(mvnRepo) else new File(args(0))
     val allJars = recursiveListFiles(directoryToScan).filter(_.getName.endsWith(".jar"))
     info(s"scanning ${allJars.length} jars")
     ASMClassVisitor.visit(allJars)
-
-    val vertexRDD = sc.parallelize(ClassVertex.toSeq)
-    vertexRDD.saveAsObjectFile(vertexFilePath)
-
-    val edgeRDD = sc.parallelize(MethodCall.toSeq)
-    edgeRDD.saveAsObjectFile(edgeFilePath)
-
-    run(sc)
   }
 
   def run(sc:SparkContext) = {
@@ -42,7 +56,14 @@ object Main {
     val edgeRDD = sc.objectFile[Edge[MethodCall]](edgeFilePath)
     val graph = Graph(vertexRDD, edgeRDD)
 
-    graph.triplets.foreach(format)
+    // Create an inverted and swapped RDD of the indegree of the map
+    // to create called stats
+    val inDegreeVertexId = graph.inDegrees.map(t => (t._2 * -1, t._1))
+    val topN = 10
+    val topCalled = inDegreeVertexId.takeOrdered(topN).map(_._2)
+    println(s"\n\n\nTop $topN called classes")
+    vertexRDD.filter(x => topCalled.contains(x._1)).map(_._2.name).foreach(println)
+    println("\n\n")
   }
 
   def info(msg: String): Unit = {
@@ -57,6 +78,13 @@ object Main {
   def recursiveListFiles(f:File): Array[File] = {
     val these = f.listFiles
     these ++ these.filter(_.isDirectory).flatMap(recursiveListFiles)
+  }
+
+  def cleanupFiles = {
+    val paths = List(vertexFilePath, edgeFilePath)
+    for (fileName <- paths;
+         file <- Some(new File(fileName)) if (file.exists());
+         deleted <- Some(FileUtils.deleteDirectory(file))) yield deleted
   }
 }
 
